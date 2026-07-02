2e6ac49a922b961f2778b57914c6687025c9dbb3
Mustafa Slama <76624245+mustafaslamv@users.noreply.github.com>
2026-07-02 03:26:17 +0300

    add shadcn

--- src/pages/Dashboard/Overview.tsx
+++ src/pages/Dashboard/Overview.tsx
@@ -1,8 +1,17 @@
+import { ChartBarDefault } from "@/components/charts/clients/example"
+
 function Overview() {
     return (
         <>
             <p className="text-muted-foreground font-sans text-sm animate-in fade-in slide-in-from-left-3 duration-600">Good morning, Coach 👋</p>
-            <h1 className="text-foreground text-6xl font-black animate-in fade-in slide-in-from-left-3 duration-600">Dashboard</h1>
+            <h1 className="mb-12 text-foreground text-6xl font-black animate-in fade-in slide-in-from-left-3 duration-600">Dashboard</h1>
+
+            <div className="flex flex-col gap-6 animate-in fade-in slide-in-from-left-5 duration-800">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    <ChartBarDefault />
+                </div>
+
+            </div>
         </>
     )
 }
